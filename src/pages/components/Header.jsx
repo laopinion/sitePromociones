46cbc560bbd5498d4e410c2e5bffa38b3a619b40
John serrano <andrey_201015@hotmail.com>
2018-06-22 12:00:42 -0500

    Terminando header desktop

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -2,17 +2,37 @@ import React from 'react'
 import './header.sass'
 import logo from '../../img/op.png'
 
+const menu = [
+  {
+    href: '/boton-1',
+    title: 'Boton #1'
+  },
+  {
+    href: '/boton-2',
+    title: 'Boton #2'
+  },
+  {
+    href: '/boton-3',
+    title: 'Boton #3'
+  }
+]
+
 const Header = () => {
   return (
     <header id='header'>
+      <hr />
       <div className='container header'>
         <figure className='logo'>
           <img src={logo} alt='laopinión' />
         </figure>
         <ul className='menu'>
-          <li>boton #1</li>
-          <li>boton #2</li>
-          <li>boton #3</li>
+          {menu.map((item, index) => {
+            return (
+              <li key={index}>
+                <a href={item.href}>{item.title}</a>
+              </li>
+            )
+          })}
         </ul>
         <form id='form'>
           <input type='text' placeholder='buscador' />
---
+++ src/pages/components/slides.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Slide = () => {
+  return (
+    <section className='Slide' />
+  )
+}
+
+export default Slide
