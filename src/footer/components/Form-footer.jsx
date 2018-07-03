4d166a37b7cf9bd98c33d1baf8c23d140e84fe06
John Serrano <andrey_201015@hotmail.com>
2018-07-03 11:35:33 -0500

    Cambios en el form del footer

--- src/footer/components/Form-footer.jsx
+++ src/footer/components/Form-footer.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const FormFooter = (props) => {
   return (
     <form id='formContactenos' onSubmit={props.handleSubscription}>
+      <p className='title'>Suscríbete y recibe las últimas promociones</p>
       <div className='form'>
         <input type='text' name='firstName' required ref={props.setRefName} />
         <label htmlFor='Nombre'>Nombre</label>
@@ -19,8 +20,8 @@ const FormFooter = (props) => {
         <button className='enviar' type='submit'>Enviar</button>
       </div>
       <div className='terminos'>
-        <input type='checkbox' name='terminos' value='Acepto de los terminos' id='terminos' required />
-        <label htmlFor='terminos'>Acepto de los terminos</label>
+        <input type='checkbox' name='terminos' value='Acepto los términos y condiciones y he leído la política de tratamiento de los datos personales' id='terminos' required />
+        <label htmlFor='terminos'>Acepto los términos y condiciones y he leído la política de tratamiento de los datos personales</label>
       </div>
     </form>
   )
